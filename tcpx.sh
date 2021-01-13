2f5efebeae784d6a7fd74e89edae029b956897f8
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-13 14:50:35 +0800

    Update tcpx.sh

--- tcpx.sh
+++ tcpx.sh
@@ -210,6 +210,8 @@ installbbr(){
 #安装BBRplus内核 4.14.129
 installbbrplus(){
 	kernel_version="4.14.160-bbrplus"
+	new_chang="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux 4.14.129-bbrplus"
+        tmp_new="GRUB_DEFAULT=\"$new_chang\""
 	bit=`uname -m`
 	rm -rf bbrplus
 	mkdir bbrplus && cd bbrplus
@@ -251,7 +253,15 @@ installbbrplus(){
 	#	reboot
 	#fi
 	echo -e "${Tip} 内核安装完毕，请参考上面的信息检查是否安装成功及手动调整内核启动顺序"
-	askuser
+	sed -i "/^GRUB_DEFAULT/c$tmp_new" /etc/default/grub
+        update-grub
+        sleep 1
+        read -p "需要重启VPS，是否现在重启 ? [Y/n] :" yn
+          [ -z "${yn}" ] && yn="y"
+             if [[ $yn == [Yy] ]]; then
+             echo -e "${Info} VPS 重启中..."
+             reboot
+             fi
 }
 
 #安装Lotserver内核
