a8a168eca9549b76bfaa33c0fc28da05ba465946
chenshuo-dr <68913746+chenshuo-dr@users.noreply.github.com>
2021-08-16 13:05:16 +0800

    Update upcore.sh

--- upcore.sh
+++ upcore.sh
@@ -53,7 +53,7 @@ reboot
 
 function start_menu(){
     clear
-    green " 更新系统内核到官方源最新版本！" 
+    echo && echo -e " 更新系统内核到官方源最新版本！ ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
     ${Green_font_prefix}1.${Font_color_suffix} Debain10
     ${Green_font_prefix}2.${Font_color_suffix} Centos7
     ${Green_font_prefix}3.${Font_color_suffix} Centos8
